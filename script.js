b59df99f1715f4751c8673249ad9a55f83fb3890
Vitória <cristina.almeida@escolar.ifrn.edu.br>
2026-05-27 20:10:08 -0300

    CSS final

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let pontosComputador = 0;
 let pontosEmpate = 0;
 let historico = [];
 let jogoTerminado = false;
+
+function atualizarPlacar() {
+  document.getElementById("pontosVoce").innerHTML = pontosVoce;
+  document.getElementById("pontosComputador").innerHTML = pontosComputador;
+  document.getElementById("pontosEmpate").innerHTML = pontosEmpate;
+}
