d20d3493d14cc4e50117c84ab61324174079f5f5
Vitória <cristina.almeida@escolar.ifrn.edu.br>
2026-05-27 20:21:38 -0300

    Zerar o jogo

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ function atualizarPlacar() {
   document.getElementById("pontosComputador").innerHTML = pontosComputador;
   document.getElementById("pontosEmpate").innerHTML = pontosEmpate;
 }
+
+
+
+function zerarJogo() {
+  pontosVoce = 0;
+  pontosComputador = 0;
+  pontosEmpate = 0;
+  historico = [];
+  jogoTerminado = false;
+
+  atualizarPlacar();
+  atualizarHistorico();
+
+  document.getElementById("mensagem").innerHTML = "Faça sua jogada para começar";
+}
+
+
+document.getElementById("pedra").addEventListener("click", function() {
+  jogar("pedra");
+});
+
+document.getElementById("papel").addEventListener("click", function() {
+  jogar("papel");
+});
+
+document.getElementById("tesoura").addEventListener("click", function() {
+  jogar("tesoura");
+});
+
+document.getElementById("zerar").addEventListener("click", function() {
+  zerarJogo();
+});
